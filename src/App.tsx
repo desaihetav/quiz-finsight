f26aeb3a6c7a00ee704ec7de044429431cbb948f
desaihd <hetav.desai20@gmail.com>
2021-05-20 22:55:55 +0530

    feat: setup tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import "./index.css";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="h-full flex justify-center items-center">
+      <h1 className="text-4xl font-bold">Hello Tailwind</h1>
+    </div>
+  );
 }
 
 export default App;
