276b0787d0d84eba4dea71fed6e7e09273aca234
desaihd <hetav.desai20@gmail.com>
2021-06-15 23:26:30 +0530

    update API link

--- src/services/quiz.ts
+++ src/services/quiz.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getAllQuizzes = async () => {
   try {
     const response = await axios.get(
-      "https://watch-finsight.desaihetav.repl.co/quiz"
+      "https://quiz-finsight.desaihetav.repl.co/quiz"
     );
     if (response.data.success) {
       return response.data.allQuizzes;
